bb186cf7355c956827975a2949601eb6f34fa667
ethanphelps <ethanrphelpsapps@gmail.com>
2022-12-03 13:24:50 -0600

    day 2

--- day-2/part1.py
+++ day-2/part1.py
@@ -1,27 +1,24 @@
 def main():
-    with open('input.txt') as f:
-        rucksacks = f.readlines()
-        count = 0
-        for r in rucksacks:
-            r = r.strip()
-            left = {}
-            right = {}
-            for c in r[:int(len(r)/2)]:
-                left[c] = 1 if c not in left else left[c] + 1
-            for c in r[int(len(r)/2):]:
-                right[c] = 1 if c not in right else right[c] + 1
-            for key in left:
-                if key in right:
-                    # print(key)
-                    count += priority(key)
-                    break
-        print(count)
+	with open('input.txt') as f:
+		rounds = f.readlines()
+		score = 0
+		for r in rounds:
+			moves = r.split()
+			score += get_score(moves)
+		print(score)
 
-def priority(c):
-    if c.islower():
-        return ord(c) - ord('a') + 1
-    elif c.isupper():
-        return ord(c) - ord('A') + 27
+def get_score(moves):
+	score = 0
+	shape_scores = {'X': 1, 'Y': 2, 'Z': 3}
+	score += shape_scores[moves[1]]
+	# transform move values to 0, 1 or 2
+	moves[0] = ord(moves[0]) - ord('A')
+	moves[1] = ord(moves[1]) - ord('X')
+	move_scores = {0:3, -1:0, 1:6, 2:0, -2:6}
+	move = moves[1] - moves[0]
+	score += move_scores[move]
+	return score
+	
 
 if __name__ == "__main__":
-    main()
+	main()
--- day-2/part2.py
+++ day-2/part2.py
@@ -1,24 +1,31 @@
 def main():
     with open('input.txt') as f:
-        rucksacks = f.readlines()
-        count = 0
-        ''' 
-        iterate over lines in sets of three
-        use a shared dictionary mapping characters to a list
-        process characters in 1st, 2nd, 3rd rucksack, appending a 1, 2 or 3 to the list value
-        in the dict depending on which rucksack in the set of 3 you're examining
-        then iterate over the items of the dict. the key that maps to a list of length 3 is the
-        item type that corresponds to the badge.
-        add the priority of that item type to the count and move on to the next set of three
-        '''
-        
-        print(count)
+        rounds = f.readlines()
+        score = 0
+        for r in rounds:
+            moves = r.split()
+            shape, outcome_points = get_shape(moves)
+            score += outcome_points
+            score += get_score(shape)
+        print(score)
 
-def priority(c):
-    if c.islower():
-        return ord(c) - ord('a') + 1
-    elif c.isupper():
-        return ord(c) - ord('A') + 27
+# returns shape to choose and score associated with outcome
+def get_shape(moves):
+    shapes_to_index = {'A': 0, 'B': 1, 'C': 2}
+    shapes = ['A', 'B', 'C']
+    if moves[1] == 'X':
+        # lose: choose shape that precedes opponent's shape (wrapping to 2 with negative index)
+        return shapes[shapes_to_index[moves[0]] - 1], 0
+    elif moves[1] == 'Y':
+        # draw: choose same shape as opponent
+        return moves[0], 3
+    else:
+        # win: choose shape that follows opponent's shape (wrapping to 0 with modulo operator)
+        return shapes[(shapes_to_index[moves[0]] + 1) % 3], 6
+
+def get_score(shape):
+	shape_scores = {'A': 1, 'B': 2, 'C': 3}
+	return shape_scores[shape]
 
 if __name__ == "__main__":
-    main()
+	main()
